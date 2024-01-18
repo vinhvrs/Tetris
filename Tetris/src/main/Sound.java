6ddb006ae2e46333192865064909732622a3513a
LE DUY HIEU <152714068+kou31053@users.noreply.github.com>
2024-01-18 22:20:39 +0700

    Update Sound.java

--- Tetris/src/main/Sound.java
+++ Tetris/src/main/Sound.java
@@ -1,44 +1,47 @@
 package main;
-import java.net.url;
+
+import java.net.URL;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineEvent;
+import javax.sound.sampled.LineEvent.Type;
+import javax.sound.sampled.LineListener;
 
 public class Sound {
-    Clip musicClip;
-    URL url[] = new URL [10];
+    private Clip musicClip;
+    private URL[] url = new URL[10];
 
     public Sound() {
-        url[0] = getClass().getResource("/bgm.wav"); //background music
-        url[0] = getClass().getResource("/rotation.wav");
-        url[0] = getClass().getResource("/delete line.wav");
-        url[0] = getClass().getResource("/gameover.wav");
-        url[0] = getClass().getResource("/touchfloor.wav");
+        url[0] = getClass().getResource("/bgm.wav"); // background music
+        url[1] = getClass().getResource("/rotation.wav");
+        url[2] = getClass().getResource("/delete line.wav");
+        url[3] = getClass().getResource("/gameover.wav");
+        url[4] = getClass().getResource("/touchfloor.wav");
     }
 
     public void play(int i, boolean music) {
         try {
             AudioInputStream ais = AudioSystem.getAudioInputStream(url[i]);
-            Clip clip = AudioInputStream.getClip();
+            Clip clip = AudioSystem.getClip();
 
             if (music) {
                 musicClip = clip;
             }
 
             clip.open(ais);
-            clip.addLineListener(new addLineListener() {
+            clip.addLineListener(new LineListener() {
                 @Override
                 public void update(LineEvent event) {
                     if (event.getType() == Type.STOP) {
-                        clip.close();  
+                        clip.close();
                     }
                 }
             });
-            ais.close();
-            clip.start()
+            clip.start();
 
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
     }
 
@@ -47,8 +50,9 @@ public class Sound {
     }
 
     public void stop() {
-        musicClip.stop();
-        musicClip.close();
-        
+        if (musicClip != null) {
+            musicClip.stop();
+            musicClip.close();
+        }
     }
 }
